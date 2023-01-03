902ad89e25ad99c4783fcecb2408f8f48b4fe2be
Hamza Kilic <hamza2358@hotmail.com>
2023-01-03 13:47:27 +0300

    Proje dosyası ekle.

---
+++ TezKontrol/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace TezKontrol
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(11, 11);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(812, 473);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(829, 445);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 39);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Rapor Ver";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(945, 445);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 39);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "OKU";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(842, 17);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "label4";
+            this.label4.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(842, 325);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(842, 211);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(842, 121);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1058, 492);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TezKontrol/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TezKontrol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> metin = new List<string>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bul();
+            onsoz();
+            grs();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { ValidateNames = true, Multiselect = false, Filter = "Word Dosyası |*.docx" })//docx dosyasını tanımlayıp sisteme yüklenmesini sağlıyoruz.
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    object readOnly = false;
+                    object visible = true;
+                    object save = false;
+                    object fileName = ofd.FileName;
+                    object newTemplate = false;
+                    object docType = 0;
+                    object missing = Type.Missing;
+                    Microsoft.Office.Interop.Word._Document document;
+                    Microsoft.Office.Interop.Word._Application application = new Microsoft.Office.Interop.Word.Application()
+                    {
+                        Visible = false
+                    };
+                    document = application.Documents.Open(ref fileName, ref missing, ref readOnly, ref missing, ref missing, ref missing,
+                        ref missing, ref missing, ref missing, ref missing, ref missing, ref visible, ref missing, ref missing, ref missing, ref missing);//Word dosyasını açıyoruz.
+                    document.ActiveWindow.Selection.WholeStory();
+                    document.ActiveWindow.Selection.Copy();
+                    IDataObject dataObject = Clipboard.GetDataObject();
+                    richTextBox1.Rtf = dataObject.GetData(DataFormats.Rtf).ToString();//word dosyasındaki bilgileri richTextBox a atama yapıyoruz.
+                    application.Quit(ref missing, ref missing, ref missing);
+                    metin.Add(richTextBox1.Text);//richTextBox daki bilgileri metin değişkenine ekliyoruz
+                }
+            }
+        }
+        public void Bul()
+        {
+            foreach (string item in metin)//metin değişkenindeki bilgileri item e aktarıyoruz
+            {
+                int sayac = 0;
+                for (int i = 0; i < item.Length; i++)
+                {
+                    if (item[i] == '“')
+                    {
+                        sayac++;
+                    }
+                    if (item[i] == '”')
+                    {
+                        sayac++;
+                    }
+                    label1.Visible = true;
+                    label4.Visible = true;
+                    int sonuc = sayac / 2;
+                    if (sonuc > 50)
+                        label4.Text = "Çift tırnak arasındaki\nkelime sayısı > 50";
+                    else
+                        label4.Text = "Çift tırnak arasındaki\nkelime sayısı < 50";
+                    label1.Text = "Çift tırnak arasındaki\nkelime sayısı: " + sonuc.ToString();
+                }
+            }
+        }
+        public void onsoz()
+        {
+            int sonuc;
+            int sonuc2 = 0;
+            int sonuc3 = 0;
+            label2.Visible = true;
+            foreach (string item in metin)//metin değişkenindeki bilgileri item e aktarıyoruz
+            {
+                sonuc = item.IndexOf("ÖNSÖZ");//Metinde ÖNSÖZ kelimesini aratıyoruz. Varsa index ini verecek yoksa -1 değeri döndürecek.
+                if (sonuc > 0)//Metin de ÖNSÖZ var mı yok mu kontrol ediyoruz 
+                {
+                    sonuc2 = item.IndexOf(".");
+                    sonuc3 = item.IndexOf("teşekkür");
+                    if (sonuc2 > sonuc3)
+                    {
+                        label2.Text = "Önsöz beyanemesinin ilk paragrafında\nteşekkür ibaresi yer alıyor.";
+                        break;
+                    }
+                    else if (sonuc3 > sonuc2)
+                    {
+                        label2.Text = "Önsöz beyanemesinin ilk paragrafında\nteşekkür ibaresi yer almıyor.";
+                        break;
+                    }
+                }
+            }
+        }
+        public void grs()
+        {
+            string a = "GİRİŞ";
+            string b = "organizasyon";
+            string c = "kapsam";
+            string d = "GENEL YAZIM KURALLARI";
+            label3.Visible = true;
+
+            int sonuc;
+
+            foreach (string item in metin)//metin değişkenindeki bilgileri item e aktarıyoruz
+            {
+                sonuc = item.IndexOf(a);
+                if (sonuc > 0)
+                {
+                    if (item.IndexOf(d) > item.IndexOf(b) && item.IndexOf(d) > item.IndexOf(c))
+                    {
+                        label3.Text = "Giriş bölümünde\norganizasyon ve kapsam bulunmaktadır.";
+                    }
+                    else
+                        label3.Text = "Giriş bölümünde\norganizasyon ve kapsam bulunmamaktadır.";
+                }
+            }
+        }
+    }
+}
